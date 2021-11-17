0b21e884fd912fd2053387d66037310097e4a412
leon1555 <leon.chisholm@keyin.com>
2021-11-17 10:42:03 -0330

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# s4sprint1-client
---
+++ src/test/java/com/keyin/ReadWebpage.java
@@ -0,0 +1,27 @@
+package com.keyin;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+
+public class ReadWebpage {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/people")).build();
+        try {
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            if (response.statusCode()==200) {
+                System.out.println(response.body());
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
